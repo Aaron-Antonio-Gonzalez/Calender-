fe8a1aa88e34e373fcca37220bfc15895bb68fee
Aaron Gonzalez <aaronglz17@gmail.com>
2021-03-03 14:50:01 -0600

    calender

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-var currentHour = Date.now().getHours();
-$('.description').each(function(){
-    var val = parseInt($(this).prop('id'));
-    if(val > currentHour && val < currentHour+6){
-        $(this).css('background-color','Blue');
-    }else if(val < currentHour && val > currentHour-6){
-        $(this).css('background-color','Red');
-    }else if(val === currentHour){
-        $(this).css('background-color','Green');
-    }else{
-        $(this).css('background-color','White');
+$('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a')); // March 1st 2021, 4:36:27 pm
+
+setInterval(function () {
+    $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+}, 1000)
+
+
+
+function localStorageFunction() {
+
+    for (let index = 0; index < numbers.length; index++) {
+        $("textarea")[index].value = localStorage.getItem("textarea" + String(index + 1));
+    }
+}
+
+$("button").on("click", function (event) {
+    event.preventDefault();
+
+    for (let index = 0; index < numbers.length; index++) {
+        localStorage.setItem('textarea' + String(index + 1), $("textarea")[index].value)
     }
 });
+
